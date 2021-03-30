f6c2530d25a5fad7acec30a1cd95fb8d6c2597db
Red3ffect <red3ffect117@gmail.com>
2021-03-30 01:45:52 -0700

    added JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// This JS file is used for the browser-bsed game, Robot Gladiators
+// Written by Tyler Evans
+
+var playerName  = window.prompt("What is your robot's name?");
+
+//console.log(playerName);
+
+// this creates a function names "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
